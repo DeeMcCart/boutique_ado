910cfc62539f96e2359a9fabb3bf1f0950edb59c
Deirdre McCarthy <deirdremc1968@gmail.com>
2023-12-04 16:00:03 +0000

    Add allauth templates and base template

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+ #   path('/', "base.html"),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 ]
